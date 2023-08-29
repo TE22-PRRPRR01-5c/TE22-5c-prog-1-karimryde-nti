90f5600ab57a6e6255e47b34f3522b8767de83f7
karye <karye.webb@gmail.com>
2023-08-29 11:05:18 +0200

    Tränat på terminalen, cw, cr, färger

---
+++ Kapitel-1/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Mitt första C#-program");
+
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Hej, vad heter du? ");
+string namn = Console.ReadLine();
+
+Console.WriteLine("Hej! " + namn + ", trevligt att se dig.");
+
+Console.ReadLine();
