527c67ac787327d2d92273c2c80fbd45b7ee23e9
karyeryde-nti <karim.ryde@ga.ntig.se>
2023-09-05 11:43:58 +0200

    Tränat på variabler och datatyper

---
+++ Kapitel-2/DatatyperVariabler/Program.cs
@@ -0,0 +1,34 @@
+﻿// Roligt program om när jag tar examen
+
+Console.Clear();
+
+// Läs in namnet
+Console.Write("Vad heter du? "); 
+string namn = Console.ReadLine();
+
+// Läs in ålder
+Console.Write("Hur gammal är du? ");
+string ålder = Console.ReadLine();
+
+// Konvertera från text till tal
+int ålderHeltal = int.Parse(ålder);
+
+// Räkna ut ålder vid examen
+int ålderVidExamen = ålderHeltal + 1;
+
+// Skriv ut hur gammal man är vid examen
+Console.WriteLine("När du " + namn + " tar examen är du " + ålderVidExamen + " år!");
+
+// Hur lång är du?
+Console.Write("Hur lång är du i meter? ");
+string längdString = Console.ReadLine();
+
+// Konvertera till decimaltal
+double längd = double.Parse(längdString);
+
+// Omvandla till centimeter
+längd = längd * 100;
+
+// Skriv ut ett svar
+//Console.WriteLine("Du är " + längd + " cm lång!");
+Console.WriteLine($"Du är {längd} cm lång");
---
+++ Labbar/Labb-1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Det här programmet räknar ut hur mycket högre Duplantis hoppar än andra.");
+
+// Läs in höjden i meter
+// TODO
+
+// Konvertera till decimaltal
+// TODO
+
+// Räkna ut hur mycket högre Duplantis hoppar
+double höjdSkillnad = höjd - 6.25;
+
+// Skriv ut resultatet
+// TODO
