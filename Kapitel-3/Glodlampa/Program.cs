f60aaaf808164ffc742a8d8bac6b985547a13a65
karyeryde-nti <karim.ryde@ga.ntig.se>
2023-09-07 14:06:20 +0200

    Tränat på if-satser

---
+++ Kapitel-3/Glodlampa/Program.cs
@@ -0,0 +1,26 @@
+﻿// Hur man lagar ett lampfel
+Console.WriteLine("Lampan fungerar inte!");
+
+// Första fråga: är lampan inkopplad?
+Console.Write("Är lampan inkopplad? (j/n)");
+string svar = Console.ReadLine();
+
+if (svar == "n")
+{
+    Console.WriteLine("Koppla in lampan!");
+}
+else
+{
+    // Andra frågan: är glödlampan utbränd?
+    Console.WriteLine("Är glödlampan utbränd? (j/n)");
+    string svar2 = Console.ReadLine();
+
+    if (svar2 == "j")
+    {
+        Console.WriteLine("Ersätt glödlampan!");
+    }
+    else
+    {
+        Console.WriteLine("Köp en ny lampa!");
+    }
+}
---
+++ Kapitel-3/Villkor/Program.cs
@@ -0,0 +1,40 @@
+﻿// Inloggningsprogram
+Console.Clear();
+
+// Läs in ett användarnamn
+Console.Write("Ange användarnamn: ");
+string anamn = Console.ReadLine();
+
+// Läs in ett lösenord
+Console.Write("Ange lösenord: ");
+string lösenord = Console.ReadLine();
+
+// Kolla om användarnamn är rätt
+/* if (anamn == "karre")
+{
+    Console.WriteLine("Trevligt att se dig igen!");
+}
+else
+{
+    Console.WriteLine("Du finns inte i systemet!");
+} */
+
+// Kolla om lösenordet är rätt
+/* if (lösenord == "qwertyui")
+{
+    Console.WriteLine("Du är inloggad!");
+}
+else
+{
+    Console.WriteLine("Lösenordet stämmer inte!");
+}
+ */
+// Vi kollar båda villkoren samtidigt
+if (anamn == "karre" && lösenord == "qwertyui")
+{
+    Console.WriteLine("Alla uppgifter stämmer! Du är inloggad!");
+}
+else
+{
+    Console.WriteLine("Användarnamnet eller lösenordet är fel!");
+}
