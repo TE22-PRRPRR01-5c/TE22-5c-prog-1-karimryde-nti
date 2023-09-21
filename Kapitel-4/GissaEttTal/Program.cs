0e6e1b6428234336a37336bf086fb7c6e2e919fa
karyeryde-nti <karim.ryde@ga.ntig.se>
2023-09-21 14:12:31 +0200

    Tränat på while-loopar

---
+++ Kapitel-4/GissaEttTal/Program.cs
@@ -0,0 +1,37 @@
+﻿// Program för att gissa ett tal mellan 1-100
+Console.Clear();
+
+// Slumpa fram ett tal
+Random tärning = new Random();
+int slumptal = tärning.Next(1, 101);
+
+// Variabel för att räkna antal gissningar
+int antal = 0;
+
+// Spelloop som avslutas när anv. gissat rätt
+while (true)
+{
+    antal += 1;
+    
+    // Be användare gissa
+    Console.Write("Gissa ett tal (1-100): ");
+    int gissning = int.Parse(Console.ReadLine());
+
+    // Kontrollera om gissningen är korrekt
+    if (gissning == slumptal)
+    {
+        Console.WriteLine("Bra gissat!");
+        break;
+    }
+    else if (gissning > slumptal)
+    {
+        Console.WriteLine("För högt!");
+    }
+    else
+    {
+        Console.WriteLine("För lågt!");
+    }
+}
+
+// Avsluta
+Console.WriteLine($"Du gissade rätt {antal} gånger.");
