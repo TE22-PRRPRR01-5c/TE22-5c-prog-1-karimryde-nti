4274068d6e8703e51bfda6e05cad016e20148d40
karyeryde-nti <karim.ryde@ga.ntig.se>
2023-11-07 11:13:10 +0100

    Tränat på debuggning

---
+++ Kapitel-5/DebugOvning1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Array med 5 platser
+string[] namnen = new string[5];
+
+// Räknare för att hålla koll på antalet sparade namn
+int antal = 0;
+
+// Inmatningsloopen
+while (antal < 6)
+{
+
+    // Fråga efter ett namn
+    Console.Write("Ange ett namn eller 'Avbryt' för att avsluta:");
+    string input = Console.ReadLine();
+
+    // Om användaren matar in "Avbryt", avsluta loopen
+    if (input == "Avbryt")
+    {
+        break;
+    }
+    // Om användaren matar in ett namn som redan finns, meddela användaren
+    else if (namnen.Contains(input))
+    {
+        Console.WriteLine("Namnet finns redan, ange ett unikt namn.");
+    }
+    // Anna, lägg till namnet i arrayen
+    else
+    {
+        namnen[antal] = input;
+        antal++;
+    }
+}
+
+// Hitta det längsta och kortaste namnet
+string längstaNamn = "";
+string kortasteNamn = "";
+
+// Loopa igenom alla namn
+foreach (var name in namnen)
+{
+    // Om namnet är längre än det längsta namnet, spara det
+    if (name.Length > längstaNamn.Length)
+    {
+        längstaNamn = name;
+    }
+
+    // Om namnet är kortare än det kortaste namnet, spara det
+    if (name.Length < kortasteNamn.Length)
+    {
+        kortasteNamn = name;
+    }
+}
+
+// Skriv ut det längsta och kortaste namnet
+Console.WriteLine("Det längsta namnet är: " + längstaNamn);
+Console.WriteLine("Det kortaste namnet är: " + kortasteNamn);
---
+++ Kapitel-5/Debugging/Program.cs
@@ -0,0 +1,19 @@
+﻿// Skriv ut namn i en array
+Console.Clear();
+Console.WriteLine("Skriv ut namn i en array");
+
+// Skapar en array med 5 platser
+string[] namnen = new string[5];
+
+// Fylla arrayen med namn
+namnen[0] = "Patric";
+namnen[1] = "Patricia";
+namnen[2] = "Kalle";
+namnen[3] = "Jonas";
+namnen[4] = "Aaron";
+
+// Skriv ut alla namnen
+foreach (var namnet in namnen)
+{
+    Console.WriteLine(namnet);
+}
