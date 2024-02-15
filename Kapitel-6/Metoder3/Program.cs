79b18bb10267bb0f1171ed9927a4b4189be4b4d9
karyeryde-nti <karim.ryde@ga.ntig.se>
2024-02-15 11:09:08 +0100

    Tränat på metoder med retur

---
+++ Kapitel-6/Metoder3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Metoder som returnerar
+Console.Clear();
+
+// En metod som summerar två värden (skicka med 2 tal)
+// Returnerar summan
+/* static int Summera(int tal1, int tal2)
+{
+    int summa = tal1 + tal2;
+    return summa;
+}
+
+// Prova metoden
+int summa = Summera(23, 45);
+Console.WriteLine(summa);
+Console.WriteLine($"Summan av 34 och 56 är {Summera(34, 56)}");
+ */
+// Den berömda metoden
+Console.Write("Ange ett heltal: ");
+//int tal = int.Parse(Console.ReadLine()); // Använd istället int.TryParse()
+int tal = LäsInHeltal();
+
+/// <summary>
+/// Metod för säker inmatning av heltal
+/// </summary>
+/// <returns>Heltalet</returns>
+static int LäsInHeltal()
+{
+    int heltal = 0;
+    while (!int.TryParse(Console.ReadLine(), out heltal))
+    {
+        Console.WriteLine("Felaktig inmatning. Försök igen!");
+    }
+    return heltal;
+}
+
+static int FrågaLäsInTal(string fråga)
+{
+    Console.WriteLine(fråga);
+    return LäsInHeltal();
+}
---
+++ Labbar/Refaktorisering/Program.cs
@@ -0,0 +1,50 @@
+﻿bool kör = true;
+
+while (kör)
+{
+    VisaMeny();
+    string val = Console.ReadLine();
+
+    switch (val)
+    {
+        case 1:
+            Console.Write("Ange cirkelns radie: ");
+            string radieStr = Console.ReadLine();
+            int radie;
+            while (!int.TryParse(radieStr, out radie))
+            {
+                Console.Write("Ange ett giltigt nummer för radien: ");
+                radieStr = Console.ReadLine();
+            }
+            Console.WriteLine($"Arean av cirkeln är: {Math.PI * radie * radie}");
+            break;
+        case 2:
+            Console.Write("Ange cirkelns radie: ");
+            radieStr = Console.ReadLine();
+            while (!int.TryParse(radieStr, out radie))
+            {
+                Console.Write("Ange ett giltigt nummer för radien: ");
+                radieStr = Console.ReadLine();
+            }
+            Console.WriteLine($"Omkretsen av cirkeln är: {2 * Math.PI * radie}");
+            break;
+        case 3:
+            kör = false;
+            break;
+        default:
+            Console.WriteLine("Ogiltigt alternativ.");
+            break;
+    }
+}
+
+/* ************************************** */
+/* ************ Metoder ***************** */
+/* ************************************** */
+static void VisaMeny()
+{
+    Console.WriteLine("\nVälj ett alternativ:");
+    Console.WriteLine("1. Beräkna area av en cirkel");
+    Console.WriteLine("2. Beräkna omkrets av en cirkel");
+    Console.WriteLine("3. Avsluta");
+    Console.Write("Ditt val: ");
+}
