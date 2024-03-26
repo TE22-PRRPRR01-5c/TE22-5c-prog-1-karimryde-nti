d00d1ece98b8a71167d5d367515a934d1949bd66
karyeryde-nti <karim.ryde@ga.ntig.se>
2024-03-26 11:10:29 +0100

    Tränat på metoder och parsing

---
+++ Labbar/CitatGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Citatgenerator");
+
+// Läs in textfilen
+List<string> rader = File.ReadAllLines("quotes.txt").ToList<string>();
+List<string> citatLista = new List<string>();
+
+// Loopa igenom raderna
+string citat = "";
+foreach (var rad in rader)
+{
+    if (rad != "")
+    {
+        citat += rad;
+    }
+    else
+    {
+        citatLista.Add(citat);
+        citat = "";
+    }
+}
+
+Console.ReadLine();
