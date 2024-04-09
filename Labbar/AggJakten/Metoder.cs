df224b740f8dc31ef84e65c42a99b1bfd2c16adb
karyeryde-nti <karim.ryde@ga.ntig.se>
2024-04-09 11:10:25 +0200

    Tränat på metoder och passera variabler per värde

---
+++ Labbar/AggJakten/Metoder.cs
@@ -0,0 +1,57 @@
+﻿/* ************************************************
+ * **************** Metoder ***********************
+ ************************************************* */
+static public class Metoder
+{
+    static public void Presentation()
+    {
+        Console.Clear();
+        Console.WriteLine("Välkommen till den stora äggjakten!");
+        Console.WriteLine("Lycka till men förätt dig inte!");
+    }
+
+    static public void VisaMeny()
+    {
+        Console.WriteLine("1. Hitta påskägg");
+        Console.WriteLine("2. Äta påskägg");
+        Console.WriteLine("3. Visa antal påskägg");
+        Console.WriteLine("4. Avsluta");
+        Console.Write("Vad väljer du? ");
+    }
+
+    static public int HittaPåskägg(int antal)
+    {
+        int slumptal = Random.Shared.Next(0, 6); // Slumpar 0-5
+        antal += slumptal;
+        Console.WriteLine($"Du hittade {slumptal} påskägg");
+        return antal;
+    }
+
+    static public int ÄtaPåskägg(int antal)
+    {
+        int slumptal = Random.Shared.Next(1, 4); // Slumpar 0-3
+
+        if (slumptal <= antal)
+        {
+            antal -= slumptal;
+            Console.WriteLine($"Du har ätit {slumptal} påskägg");
+        }
+        else
+        {
+            antal = 0;
+            Console.WriteLine($"Du har ätit alla dina påskägg");
+        }
+
+        if (antal >= 3)
+        {
+            Console.WriteLine($"Oj, nu får du ont i magen!");
+        }
+
+        return antal;
+    }
+
+    static public void VisaAntalPåskägg(int antal)
+    {
+        Console.WriteLine($"Du har {antal} påskägg kvar i korgen");
+    }
+}
---
+++ Labbar/AggJakten/Program.cs
@@ -0,0 +1,42 @@
+﻿/* *************************************************
+ * Namn: Påskäggsjakten
+ * Beskrivning: Ett program som simulerar en påskäggsjakt
+ * Av: [Ditt namn]
+ * Datum: 2021-09-01
+ *********************************************** */
+
+Metoder.Presentation();
+
+// Initiera antal påskägg till 0
+int antalPåskägg = 0;
+
+// Programloop
+while (true)
+{
+    // Visa en meny för användaren
+    Metoder.VisaMeny();
+    string val = Console.ReadLine();
+
+    // Hantera användarens val
+    if (val == "1")
+    {
+        antalPåskägg = Metoder.HittaPåskägg(antalPåskägg);
+    }
+    else if (val == "2")
+    {
+        antalPåskägg = Metoder.ÄtaPåskägg(antalPåskägg);
+    }
+    else if (val == "3")
+    {
+        Metoder.VisaAntalPåskägg(antalPåskägg);
+    }
+    else if (val == "4")
+    {
+        Console.WriteLine("Tack för den här gången");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du angav ett felaktigt val, försök igen.");
+    }
+}
